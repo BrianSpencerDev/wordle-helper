7be066d23469f14c6ec32f5c4a4d2ef86dedb7dc
Brian Spencer <brsj13@gmail.com>
2022-06-13 22:06:13 -0700

    add how to modal

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      'html, body': {
+        bg: 'black',
+        color: 'white'
+      }
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component className=".h-screen .w-screen" {...pageProps} />
+  return <ChakraProvider theme={theme}>
+    <Component className=".h-screen .w-screen" {...pageProps} />
+  </ChakraProvider>
+  
 }
 
 export default MyApp
